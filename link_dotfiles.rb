6f15a5aa230a0b57db26143066a27f843d27908f
Harry <harry.robertson@gmail.com>
2011-02-15 23:52:52 -0800

    First commit, with dmacdougall's link_dotfiles script.

---
+++ link_dotfiles.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+DOTDIR = ".dotfiles"
+Dir.chdir(File::expand_path("~"))
+Dir.foreach(DOTDIR) do |file|
+  if file.match(/(\.[a-z]+)/) && file != ".git"
+    system("rm #{file}")
+    system("ln -Fs #{DOTDIR}/#{file} #{file}")
+    puts "#{file} => #{DOTDIR}/#{file}"
+  end
+end
