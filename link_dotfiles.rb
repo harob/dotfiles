ec1d15faebdc5832869ee9a23504a8476808696a
Harry <harry.robertson@gmail.com>
2011-06-08 14:50:49 -0700

    Fixing up link_dotfiles script

--- link_dotfiles.rb
+++ link_dotfiles.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
 DOTDIR = "dotfiles"
-Dir.chdir(File::expand_path("~"))
+
+Dir.chdir File::expand_path("~")
 Dir.foreach(DOTDIR) do |file|
-  if file.match(/(\.[a-z]+)/) && file != ".git"
-    system("rm #{file}")
-    system("ln -Fs #{DOTDIR}/#{file} #{file}")
+  if file =~ /^\.[a-z]+/ && file != ".git"
+    `rm #{file}`
+    `ln -Fs #{DOTDIR}/#{file} #{file}`
     puts "#{file} => #{DOTDIR}/#{file}"
   end
 end
