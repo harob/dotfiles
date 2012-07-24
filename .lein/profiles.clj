9c758c65da459c86373f5aec0b1e7b646fba9afa
Harry <harry.robertson@gmail.com>
2012-07-24 10:17:29 -0700

    WIP on in-directory dotfiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,2 @@
+{:user {:plugins [[lein-tarsier "0.9.2"]]}}
+
--- link_dotfiles.rb
+++ link_dotfiles.rb
@@ -5,8 +5,12 @@ DOTDIR = "dotfiles"
 Dir.chdir File::expand_path("~")
 Dir.foreach(DOTDIR) do |file|
   if file =~ /^\.[a-z]+/ && file != ".git"
-    `rm #{file}`
-    `ln -Fs #{DOTDIR}/#{file} #{file}`
-    puts "#{file} => #{DOTDIR}/#{file}"
+    if File.directory? file
+      # TODO(harry) Recursively create directories and symlinks, eg for .lein/profiles.clj
+    else
+      `rm #{file}`
+      `ln -Fs #{DOTDIR}/#{file} #{file}`
+      puts "#{file} => #{DOTDIR}/#{file}"
+    end
   end
 end
